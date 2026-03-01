e955a7b7e4063246a00497a82345b9d18ffb5a50
0865098 <0865098@my.scccd.edu>
2026-03-01 14:28:09 -0800

    Changed input architecture from single line to file read. argv[1] is the filename you'd like to compile (interpret) without the file extension. When creating a file for this, use the extension .it .

--- main.cc
+++ main.cc
@@ -1,5 +1,9 @@
+#include <cstdio>
 #include <cstdlib>
+#include <fstream>
+#include <sstream>
 #include <stdexcept>
+#include <string>
 #include <vector>
 #include "/public/read.h" // IWYU pragma: keep
 #include "node.h"
@@ -11,11 +15,28 @@ using namespace std;
 
 using ContextMap = std::unordered_map<string, DeclarationNode*>;
 
-int main() {
+string fileTarget(const string &filename) {
+	ifstream ifs(filename + ".it");
+	if(!ifs) {
+		throw runtime_error("File " + filename + " is not found");
+	}	
+	stringstream ss; 
+	ss << ifs.rdbuf();	
+
+	return ss.str();
+}
+
+int main(int argc, char** argv) {
 	vector<Token> tokens;
 	string buffer;
 	ContextMap Context;
-	getline(cin, buffer);
+
+	if(argc > 1) {
+		buffer = fileTarget(string(argv[1]));
+	} else {
+		throw runtime_error("Provide a filename you'd like to \"compile\". Do not include the file extension.");
+	}
+
 	Lexer l{buffer};
 	tokens = l.tokenizeBuffer();
 
